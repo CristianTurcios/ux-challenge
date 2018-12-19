e4e4d58a6513e7b65c94f8a582a66c2952176c23
Cristian Turcios <cristian.turcios@laureate.net>
2018-12-18 22:44:24 -0600

    finish trend card

--- src/app/trend/trend.component.ts
+++ src/app/trend/trend.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trend.component.scss']
 })
 export class TrendComponent implements OnInit {
+  percentages: Array<string>;
+  // pastEvents: Array<Event>;
 
   constructor() { }
 
   ngOnInit() {
+    this.percentages = ['70%', '60%', '80%',  '100%',  '120%', '150%', '120%', '100%', '80%', '70%', '60%', '30%'];
+    // this.percentages = [{ 0: '70%', 1: '60%' }];
+
   }
 
 }
